77e016099b5b649f82989acfc5abd133188232af
SimonaR07 <sristomanova32@gmail.com>
2024-01-19 20:55:07 +0100

    Add new java program

---
+++ src/MyClass.java
@@ -0,0 +1,10 @@
+
+public class MyClass {
+	public static void main(String[] args) {
+		Animal dog = new Animal();
+		dog.bark();
+
+	}
+
+
+}
---
+++ src/Zadaca.java
@@ -0,0 +1,12 @@
+import Proekt3.zadaca;
+
+public class Zadaca {
+	public static void main(String[] args) {
+		zadaca dog = new zadaca();
+		dog.bark();
+		zadaca resi=new zadaca();
+		resi.zadaca();
+
+	}
+
+}
